e8429cd2adba6e37699764eab74f208f8e74fa28
Bruce Shih <sp1986@gmail.com>
2020-08-27 21:24:47 +0800

    remove my own firebase config

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -2,14 +2,9 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
+// TODO: use your firebase config here
 const firebaseConfig = {
-  apiKey: 'AIzaSyDT0-GNlFmlkO93zzgLow7U_5KlNYql6aU',
-  authDomain: 'cherri-interview.firebaseapp.com',
-  databaseURL: 'https://cherri-interview.firebaseio.com',
-  projectId: 'cherri-interview',
-  storageBucket: 'cherri-interview.appspot.com',
-  messagingSenderId: '124960204862',
-  appId: '1:124960204862:web:e1136df4525034048dc96f'
+
 };
 
 firebase.initializeApp(firebaseConfig);
