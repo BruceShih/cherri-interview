5a83d9e46c1b3012a7440ad0306f97a5c7ba0864
Bruce Shih <sp1986@gmail.com>
2020-08-27 21:34:40 +0800

    resolve publicPath for hosting

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/cherri/' : '/',
   transpileDependencies: ['vuetify'],
-
   pluginOptions: {
     i18n: {
       locale: 'zh-TW',
